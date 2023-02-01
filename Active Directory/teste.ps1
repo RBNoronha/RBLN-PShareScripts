e406dc09880978a480af4c4806d53704d643c1ec
Renan Noronha <91945458+RBNoronha@users.noreply.github.com>
2023-02-01 15:32:08 -0300

    Update teste.ps1

--- Active Directory/teste.ps1
+++ Active Directory/teste.ps1
@@ -2,11 +2,7 @@ param(
     [Parameter(Mandatory,
         HelpMessage = "Enter the pattern for filtering groups"
     )]
-    $Pattern,
-    [Parameter(Mandatory = $false,
-        HelpMessage = "Enter the range for filtering groups"
-    )]
-    $Range = 150
+    $Pattern
 )
 
 # Excel file path
@@ -21,19 +17,19 @@ $ClientSecretCredential = New-Object -TypeName 'System.Management.Automation.PSC
 # Connecting to Microsoft Graph
 Connect-MgGraph -TenantId $TenantID -ClientSecretCredential $ClientSecretCredential | Out-Null
 
-# Getting all the groups with displayname starting with the provided pattern
-$Groups = Get-MgGroup -filter "startswith(displayname,'$Pattern')" -Top 2000
+# Getting all the groups with display name starting with the provided pattern
+$Groups = Get-MgGroup -Filter "startswith(displayname,'$Pattern')" -Top 2000
 
-# Looping through all the filtered groups and exporting their members to the csv files
+# Looping through all the filtered groups and exporting their members to the Excel files
 $Count = 0
 foreach ($Group in $Groups) {
     $Count += 1
     $WorkSheetName = "Group$($Count)"
     Try{
-    (Get-MgGroupMember -GroupId $Group.id -Top $Range).AdditionalProperties | `
-            Select-Object @{n = "DisplayName"; e = { $_.displayName } }, @{n = "UserprincipalName"; e = { $_.userPrincipalName } } |`
-            Export-Excel -path $ExcelFilePath -WorksheetName $WorkSheetName -Append -TableStyle 'Medium16' `
-            -Title $Group.Displayname -TitleSize 14 -TitleBold
+    (Get-MgGroupMember -GroupId $Group.id -Top 150).AdditionalProperties | `
+            Select-Object @{n = "DisplayName"; e = { $_.displayName } }, @{n = "UserPrincipalName"; e = { $_.userPrincipalName } } |`
+            Export-Excel -Path $ExcelFilePath -WorksheetName $WorkSheetName -Append -TableStyle 'Medium16' `
+            -Title $Group.DisplayName -TitleSize 14 -TitleBold
     }
     Catch {
         Write-Host $_.Exception.Message -ForegroundColor 'Red'
